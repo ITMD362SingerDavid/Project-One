db4afd7f6c1cf6f7e1bb6a3165987b4a246d454b
David Singer <dsinger1@hawk.iit.edu>
2021-03-16 21:31:03 -0500

    Keep instagram information hidden unless it is checked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+var hiddenInsta = document.getElementById("hidden-instagram");
+var chkInsta = document.getElementById("instagram");
+
+//Keep instagram information hidden unless the box is checked
+function showInsta(){
+  if(chkInsta.checked == false){
+    hiddenInsta.style.display = "none";
+  }
+}
